024fd6d3a21272a5838b30fa1c1914a939517089
RamssCR <ramsesgpm@gmail.com>
2025-08-26 09:21:54 -0500

    test: creadas pruebas de ejemplo

---
+++ test/example.test.js
@@ -0,0 +1,32 @@
+import { describe, expect, test, vi } from 'vitest'
+
+describe('Example Test Suite', () => {
+  test('should mock console methods', () => {
+    const consoleError = vi.spyOn(console, 'error')
+    const consoleLog = vi.spyOn(console, 'log')
+
+    console.error('This is an error')
+    console.log('This is a log')
+
+    expect(consoleError).toHaveBeenCalledWith('This is an error')
+    expect(consoleLog).toHaveBeenCalledWith('This is a log')
+
+    consoleError.mockRestore()
+    consoleLog.mockRestore()
+  })
+
+  test('should mock browser APIs', () => {
+    expect(window.requestAnimationFrame).toBeDefined()
+    expect(window.ResizeObserver).toBeDefined()
+    expect(window.IntersectionObserver).toBeDefined()
+  })
+
+  test('should return a promise that resolves to a value', () => {
+    const delayTime = 25
+    const promise = new Promise((resolve) => {
+      setTimeout(() => resolve('resolved value'), delayTime)
+    })
+
+    return expect(promise).resolves.toBe('resolved value')
+  })
+})
