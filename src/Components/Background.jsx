d822f712bc23f8fd9b39da2520e48ca6fc923640
Tomsam321 <elmastomi9@gmail.com>
2025-09-07 12:55:46 -0500

    feat: add background component
    Add Background.jsx with Background, Background2, and BackgroundCTA components

---
+++ src/Components/Background.jsx
@@ -0,0 +1,30 @@
+/**
+ * Componente Background separado.
+ * @returns {import('react').JSX.Element} Componente Background.
+ */
+const Background = () => (
+  <img
+    className="absolute -top-40 -right-20 -z-10 w-[1000px] h-auto overflow-hidden"
+    src="/images/images/bg-tablet-pattern.svg"
+    alt="bg"
+  />
+)
+
+const Background2 = () => (
+  <img
+    className="absolute top-[1400px] -left-[500px] -z-10 w-[1000px] h-auto"
+    src="/images/images/bg-tablet-pattern.svg"
+    alt="bg-2"
+  />
+)
+
+const BackgroundCTA = () => (
+  <img
+    className="absolute -top-10 right-60 md:right-130 z-10 opacity-10 brightness-150 w-[600px] h-auto"
+    src="/images/images/bg-tablet-pattern.svg"
+    alt="bg-Cta"
+  />
+)
+
+export default Background
+export { Background2, BackgroundCTA }
